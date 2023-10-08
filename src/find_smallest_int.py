46e69b666c179192fc7ec1740d20df9c2ef50155
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2023-10-08 10:07:54 -0600

    Clear white space in find_smallest_int.py

--- src/find_smallest_int.py
+++ src/find_smallest_int.py
@@ -7,7 +7,6 @@ Given [34, 15, 88, 2] your solution will return 2
 Given [34, -345, -1, 100] your solution will return -345
 You can assume, for the purpose of this kata, that the supplied array will not be empty.
 '''
-
 def find_smallest_int(arr):
     smallest = arr[0]
     
